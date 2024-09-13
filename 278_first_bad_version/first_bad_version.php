7abef175db1e26ee61c63cc5065c7e89666556db
tiennham <hongtiennham@gmail.com>
2024-09-13 11:58:03 +0700

    upload missing file

--- 278_first_bad_version/first_bad_version.php
+++ 278_first_bad_version/first_bad_version.php
@@ -1,2 +1,22 @@
 <?php
 
+class Solution extends VersionControl {
+    /**
+     * @param Integer $n
+     * @return Integer
+     */
+    function firstBadVersion($n) {
+        $left = 0;
+        $right = $n;
+
+        while ($left < $right) {
+            $middle = floor(($left + $right) / 2);
+            if($this->isBadVersion($middle)){
+                $right = $middle;
+            } else {
+                $left = $middle + 1;
+            }
+        }
+        return $left;
+    }
+}
